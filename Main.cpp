12e44b00878d8c1ee492996beb2072d13b1a8aca
Thebristi <bristibaral10@gmail.com>
2026-08-09 23:16:36 +0545

    Create Main.cpp
    Add includes

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+// Project name: Cafe Billing System Using C++
+// Object-Oriented Programming
+// A Console-based billing system for a small cafe.
+//This demonstrates classes/objects, encapsulation, constructors, and inheritance.
+
+#include <iostream>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<limits>
+using namespace std;
+// MenuItem: Stores the details of a single food/drink item
+
+class MenuItem{
+    private:
+    int id;
+    string name;
+    double price;
+
+   
+}
